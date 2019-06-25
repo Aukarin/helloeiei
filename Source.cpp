ba0c975bc2576871521d0a6e91183894b03bebb9
Aukarin <6206021622062@fitm.kmutnb.ac.th>
2019-06-25 20:22:59 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-	cout << "Hello world. \n";
+	string Name = " Aukarin Rungsatjaborirak";
+	short scores1 = 100;
+	short scores2 = 95;
+	char  Grade = 'A';
+	float GPA = 4;
+
+	cout << "  My Name is : " << Name << endl;
+	cout << " My scores 1 : " << scores1 << " point" << endl;
+	cout << " My scores 2 : " << scores2 << " point" << endl;
+	cout << "       Total : " << scores1 + scores2 << "point" << endl;
+	cout << "       Grade : " << Grade << endl;
+	cout << "         GPA : " << GPA << endl;
+
 	return(0);
 }
