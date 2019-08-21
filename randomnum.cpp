d4229f198e917010e270a55cd957052ece240cb9
Aukarin <6206021622062@fitm.kmutnb.ac.th>
2019-08-21 19:48:30 +0700

    Create randomnum.cpp

---
+++ randomnum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+int checkNum();
+int num,i=0;
+int numrand;
+int main()
+{
+	srand(time(NULL));
+	numrand = 1 + rand() % 10 ;
+	cout << "####Welcome to guessing number game####\n";
+	cout << "Secret number has been chosen \n";
+	do{
+		i++;
+		cout << "Guess the number (1 to 10): ";
+		cin >> num;
+		checkNum();
+	} while (num != numrand);
+}
+int checkNum()
+{
+	if (num > numrand){
+		cout << "The secret number is lower \n";
+	}
+	if (num < numrand){
+		cout << "The secret number is higher \n";
+	}
+	if (num == numrand){
+		cout << "Congratulations! \n";
+		cout << "The secret number is " << numrand << endl;
+		cout << "You made "<< i <<" guesses\n";
+	}
+	return (0);
+}
