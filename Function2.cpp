5ebc823b72c993b377117eb37554dbe4cf48660a
Aukarin <6206021622062@fitm.kmutnb.ac.th>
2019-09-02 19:29:35 +0700

    Update Function2.cpp

--- Function2.cpp
+++ Function2.cpp
@@ -1,47 +1,50 @@
 #include <iostream>
 #include <string>
+#include<iomanip>
 using namespace std;
-int update_balance(string , float , float &balance);
+int update_balance(string, float, float &balance);
 int main()
 {
-	
+
 	float y, z, dollars, balance = 10000;
 	string x;
 	char command;
 	int q;
-	
+
 	cout << "Exit with 0 \n";
-	cout << "Your balance = " << balance << endl;
+	cout << "Your balance = " << balance  << endl;
+
 	do{
 		
 		cout << "Input command (1 or withdraw, 2 deposit): ";
 		cin >> command;
+		if (command == '0')break;
 		cout << "Input amount : ";
 		cin >> dollars;
 		x = command;
 		y = dollars;
-		
-		update_balance(x, y, z);
-		cout << "Your balance = " << update_balance(x, y, balance);
-		cout << endl;
 
-	} while (dollars != 0) ;
-	cout << "End";
+		update_balance(x, y, z);
+		cout << "Your balance = " <<update_balance(x, y, balance);
+		cout  << endl;
+
+	} while (command != '0');
+	cout << "End\n";
 }
 int update_balance(string command, float dollars, float &balance)
 {
-	
-	
+
+
 	if (command == "1")
 	{
-		balance= balance - dollars;
+		balance = balance - dollars;
 
 	}
 
 	if (command == "2")
 	{
-		balance= balance + dollars;
+		balance = balance + dollars;
 	}
-	
+
 	return(balance);
 }
